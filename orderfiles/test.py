fc194b2bd28dc19a0c977694e693234e8178280f
Miguel Quintana <miqo1385@gmail.com>
2022-05-30 18:10:30 -0700

    second commit

---
+++ orderfiles/test.py
@@ -0,0 +1,24 @@
+import unittest
+from customer import Customer
+
+class TestCustomer(unittest.TestCase):
+    def setUp(self):
+        self.c=Customer(12345678912345, 'Marcos Aurelio')
+
+    def test_Ccnumber(self):
+        self.assertEqual(self.c.getCcNumber(), 12345678912345)
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
